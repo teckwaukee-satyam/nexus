3866e23008c1c0192e8b67e5c8f81c5718901dd4
techwaukee-satyam <satyam.kumar@techwaukee.com>
2025-05-07 15:33:39 +0530

    home page modified again

---
+++ Pages/_Navbar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nexus.Pages
+{
+    public class _NavbarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
